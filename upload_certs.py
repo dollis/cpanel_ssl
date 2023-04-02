4c681914a1bd05d36ee3932915ab1f73ec227e99
David Ollis <david@ollis.id.au>
2023-04-02 09:05:15 +0000

    Initial Commit
    Uploads a certificate/key/ca bundle from acmetool live directory

---
+++ upload_certs.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import requests
+import argparse
+import os
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Upload certificate to CPANEL Host")
+    parser.add_argument("--domain", "-d", required=True)
+    args = parser.parse_args()
+    load_dotenv()
+    cpanel_username = os.environ.get("USERNAME")
+    cpanel_api_token = os.environ.get("API_TOKEN")
+    cpanel_base_url = os.environ.get("BASE_URL")
+    live_base_dir = os.environ.get("LIVE_DIR")
+    live_base_path = Path(live_base_dir) / args.domain
+
+    certpath = live_base_path / "cert"
+    with certpath.open() as certfile:
+        cert = certfile.read()
+
+    keypath = live_base_path / "privkey"
+    with keypath.open() as keyfile:
+        key = keyfile.read()
+
+    chainpath = live_base_path / "chain"
+    with chainpath.open() as chainfile:
+        chain = chainfile.read()
+
+    query = {"cert": cert, "key": key, "cabundle": chain, "domain": args.domain}
+
+    url = f"https://{cpanel_base_url}/execute/SSL/install_ssl"
+    headers = {
+        "Authorization": f"cpanel {cpanel_username}:{cpanel_api_token}",
+        "Accept": "application/json",
+    }
+    r = requests.get(url, params=query, headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        raise SystemExit(f"request to {url} failed with status {r.status_code}")
+
+    response_data = r.json()
+    if response_data["status"] == 0:
+        raise SystemExit(response_data["errors"][0])
+
+
+if __name__ == "__main__":
+    main()
